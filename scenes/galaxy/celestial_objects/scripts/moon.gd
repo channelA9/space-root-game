e3d4a616abae129636f66ba3f3f7ccdbf09ae9cc
channel9a <shaun.colegado@gmail.com>
2023-01-31 23:08:22 -0800

    added moons, visual orbit

--- scenes/galaxy/celestial_objects/scripts/moon.gd
+++ scenes/galaxy/celestial_objects/scripts/moon.gd
@@ -1,10 +1,9 @@
 extends KinematicBody2D
 
-signal mouse_over_planet(radius, angle_from, angle_to, color)
-onready var rotation_point = Physics.universe_center
+onready var rotation_point = get_parent().global_position
 onready var distance_from_point = position.distance_to(rotation_point)
 onready var theta = global_position.angle_to_point(rotation_point)
-onready var speed = Physics.rng.randf_range(1,5)/(distance_from_point/100)
+onready var speed = 1
 
 var mouseOver = false
 # Called when the node enters the scene tree for the first time.
@@ -15,11 +14,3 @@ func _process(delta):
 	global_position = rotation_point
 	global_position += Vector2(cos(theta), sin(theta)) * distance_from_point
 	theta += (delta*Physics.speed_scale*speed)
-
-func _on_planet_mouse_entered():
-	emit_signal("mouse_over_planet", distance_from_point, 0, 360, Color(255,255,255))
-	print("Mouse in")
-
-func _on_planet_mouse_exited():
-	print("Mouse out")
-		
--- scenes/galaxy/celestial_objects/scripts/physics.gd
+++ scenes/galaxy/celestial_objects/scripts/physics.gd
@@ -3,7 +3,7 @@ extends Node2D
 var rng = RandomNumberGenerator.new()
 
 var universe_center = Vector2(0,0)
-var speed_scale = 365/60/60
+var speed_scale = 1
 class celestial_body:
 	var radius
 	var distance
